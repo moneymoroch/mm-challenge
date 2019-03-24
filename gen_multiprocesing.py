bfb8f1191e414f1546c4c01239059366e40a8682
M Moroch <moroch828@gmail.com>
2019-03-24 19:55:15 -0400

    updates

---
+++ gen_multiprocesing.py
@@ -0,0 +1,81 @@
+from queue import Queue
+import os
+import random
+import string
+from threading import Thread
+import time
+import itertools
+import csv
+import sys
+import multiprocessing
+
+finished = multiprocessing.Value('i', False)
+class MMChallengeProcess:
+    def __init__(self):
+        self.jobs = multiprocessing.Queue()
+        self.outfile = 'data.csv'
+        self.outsize = 200 # MB
+        self.file = open(self.outfile, 'w')
+    
+        self.start = 0
+        self.end = 20000
+        self.offset = 20000
+
+    ''' Function to generate row specified in document '''
+    def generateRow(self, count):
+        string1 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
+        string2 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
+        data = [str(count), str(random.randint(1,10)), string1, string2]
+        return ','.join(data) + '\n'
+
+    ''' Generates data for range of primary keys given. For example, if 2000, 4000 are used, 
+        function will generate 2000 rows starting with id 2000 and ending at 40000. 
+    '''
+    def calculateAndWrite(self, start, end):
+            temp = ''
+            for i in range(start, end):
+                temp += (self.generateRow(i))
+            self.file.write(temp)
+
+    ''' Pops jobs off queue to process '''     
+    def processJobs(self, jobs):
+        while not self.jobs.empty() and not bool(finished.value):
+            window = self.jobs.get()
+            start = window[0]
+            end = window[1]
+            self.calculateAndWrite(start, end)
+
+
+    ''' Worker that will continuosly add chunks of primary id's to calculate until file size if reached '''
+    def loadJobs(self):
+        while (os.path.getsize(self.outfile)//1024**2) < self.outsize:
+                self.jobs.put([self.start, self.end])
+                self.start = self.end 
+                self.end += self.offset
+
+        with finished.get_lock():
+            finished.value = True
+
+        print("--- %s seconds ---" % (time.time() - start_time))
+        sys.exit()
+
+    ''' Main function '''
+    def run(self):
+        t = multiprocessing.Process(target=self.loadJobs, args=())
+        t.start()
+
+        for work in range(0, 30):
+            worker = multiprocessing.Process(target=self.processJobs, args=(self.jobs,))
+            worker.start()
+           
+        
+if __name__ == '__main__':
+    print("Generating File...")
+    start_time = time.time()
+
+    mm = MMChallengeProcess()
+    mm.run()
+   
+
+   
+    
---
+++ gen_multithreading.py
@@ -0,0 +1,55 @@
+from queue import Queue
+import os
+import random
+import string
+from threading import Thread
+import time
+import itertools
+import csv
+import sys
+
+class MMChallengeThreaded:
+    def __init__(self):
+        self.q = Queue()
+        self.outfile = 'data.csv'
+        self.outsize = 500 # MB
+        self.file = open(self.outfile, 'w')
+        self.finished = False
+        self.start = 0
+        self.end = 1000
+        self.offset = 1000
+
+    def generateRow(self, count):
+        string1 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
+        string2 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
+        data = [str(count), str(random.randint(1,10)), string1, string2]
+        return ','.join(data) + '\n'
+        #return data
+
+    def makeRows(self, start, end):
+        temp = ''
+        for i in range(start, end):
+            temp += (self.generateRow(i))
+        self.file.write(temp)
+
+    def run(self):
+        while (os.path.getsize(self.outfile)//1024**2) < self.outsize:
+
+                t = Thread(target = self.makeRows, args=(self.start, self.end,))
+                self.start = self.end 
+                self.end += self.offset
+                t.daemon = True
+                t.start()
+        
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    print("Generating File...")
+    start_time = time.time()
+
+    mm = MMChallengeThreaded()
+    mm.run()
+    
+
+   
+    
