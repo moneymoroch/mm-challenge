bcf2dd722f87906ebbc309c9974baa2e869e209f
M Moroch <moroch828@gmail.com>
2019-03-24 17:58:03 -0400

    multi

--- gen_multi.py
+++ gen_multi.py
@@ -6,37 +6,75 @@ from threading import Thread
 import time
 import itertools
 import csv
-q = Queue()
-outfile = 'data.csv'
-outsize = 1 # MB
-file = open(outfile, 'w')
-wtr = csv.writer(file)
+import sys
 
-def getSize(filename):
-    st = os.stat(filename)
-    return st.st_size
+class MMChallengeThreaded:
+    def __init__(self):
+        self.q = Queue()
+        self.outfile = 'data.csv'
+        self.outsize = 50 # MB
+        self.file = open(self.outfile, 'w')
+        self.finished = False
+        self.start = 0
+        self.end = 10000
+        self.offset = 10000
 
-def generate_row(count):
-    string1 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
-    string2 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
-    data = [str(count), str(random.randint(1,10)), string1, string2]
-    #return ','.join(data) + '\n'
-    return data
+    def getSize(self, filename):
+        st = os.stat(filename)
+        return st.st_size
 
-def make_rows():
-    count = 0
-    while (os.path.getsize(outfile)//1000000) < outsize:
-    #while count < 100000:
-        q.put(generate_row(count))
-        count = count + 1
+    def generateRow(self, count):
+        string1 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
+        string2 = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1,32)))
+        data = [str(count), str(random.randint(1,10)), string1, string2]
+        return ','.join(data) + '\n'
+        #return data
 
-def write_file():
-   while not q.empty():
-       wtr.writerow(q.get())
-       #print(q.get())
+    def makeRows(self, start, end):
+        temp = ''
+        for i in range(start, end):
+            temp += (self.generateRow(i))
+        self.file.write(temp)
 
+
+    def writeStream(self):
+        ''' Set stdout to file '''
+        sys.stdout = open(self.outfile, 'w')
+
+        with open(self.outfile, 'w') as self.file:
+            while (os.path.getsize(self.outfile)//1024**2) < self.outsize:
+                while not self.q.empty():
+                    print(self.q.get())
+
+        self.finished = True
+        sys.stdout = sys.__stdout__
+        print("--- %s seconds ---" % (time.time() - start_time))
+        cleanup_stop_thread()
+        sys.exit()
+
+    def run(self):
+
+        while (os.path.getsize(self.outfile)//1024**2) < self.outsize:
+
+                print('starting thread')
+                print(self.start, self.offset)
+                t = Thread(target = self.makeRows, args=(self.start, self.end,))
+                self.start = self.end 
+                self.end += self.end
+                t.start()
+                
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    def collect(self):
+        Thread(target = self.writeStream, args=(self.start, self.offset,))
 
 if __name__ == '__main__':
-    Thread(target = make_rows).start()
-    Thread(target = write_file).start()
+    start_time = time.time()
+
+    mm = MMChallengeThreaded()
+    mm.run()
+    mm.collect()
+    
+
+   
     
