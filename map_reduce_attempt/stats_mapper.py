c96c92e917f9fa88ce3f61245aa9c7694fde96e9
M Moroch <moroch828@gmail.com>
2019-03-28 16:20:09 -0400

    Syntax

--- map_reduce_attempt/stats_mapper.py
+++ map_reduce_attempt/stats_mapper.py
@@ -1,11 +1,16 @@
 '''
-Attemt to speed up calculation of integer distribution and vowel count by simulating a mapreduce job.
-Tried spawning a process every few thousands rows and piping max vowels of each process to stats_reducer.py. 
-Ultimately, this didnt speed it up, although computatations are done in parallel, the CSV read is not, and the speed
-is bounded by this. 
+Attemt to speed up calculation of vowel count by simulating a mapreduce job. 
 
-I think you'd need to split the csv up first to actually read in parallel, or just use HDFS. 
-https://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/
+To run on HDFS:
+
+bin/hadoop jar /usr/local/Cellar/hadoop/3.1.1/libexec/share/hadoop/tools/lib/hadoop-*streaming*.jar -D mapred.reduce.tasks=16 \
+-file /Users/zuba/mm-challenge/map_reduce_attempt/stats_mapper.py \
+-mapper  'python3 stats_mapper.py'  \
+-file  /Users/zuba/mm-challenge/map_reduce_attempt/stats_reducer.py  \
+-reducer 'python3 stats_reducer.py'  \
+-input /user/zuba/source/source.csv \
+-output /user/zuba/source/output
+
 
 cat source.csv | python3 map_reduce_attempt/stats_mapper.py | python3 map_reduce_attempt/stats_reducer.py
 '''
